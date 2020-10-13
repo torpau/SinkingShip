ec0bdeea0ee301a289e731aac7ccfabe2db286af
Maria Hermansson <mdhaxx0r@gmail.com>
2020-10-13 16:25:58 +0200

    gamebord progress

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -5,42 +5,77 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class GameBoard {
+    public static JFrame f = new JFrame();
+    private static final int frameWidth = 720;
+    private static final int frameHeight = 400;
+    private static final Font font = new Font("Verdana", Font.PLAIN, 12);
+    private static final Border blackline = BorderFactory.createLineBorder(Color.black);
 
-    public GameBoard(){
+    public GameBoard() {
 
     }
 
-    static void initGameBoard(){
-        JFrame f=new JFrame();//creating instance of JFrame
-        Border blackline = BorderFactory.createLineBorder(Color.black);
-        Font font = new Font("Verdana", Font.PLAIN, 12);
-
-
-        int frameWidth= 900;
-        int frameHeight = 400;
-        f.setBounds(200, 150, frameWidth, frameHeight);
+    static void initGameBoard() {
+        GameBoard.f.setBounds(200, 150, frameWidth, frameHeight);
+        GameBoard.f.setLayout(null);//using no layout managers
+        GameBoard.f.setVisible(true);//making the frame visible
         //f.getJMenuBar();
-        f.setLayout(null);//using no layout managers
-        f.setVisible(true);//making the frame visible
 
-        JButton b=new JButton("");//creating instance of JButton
+        JButton b = new JButton("");//creating instance of JButton
         b.setBounds((frameWidth-58),30,25, 25);//x axis, y axis, width, height
         b.setEnabled(false);
 
-        JLabel l = new JLabel("A");
-        l.setBounds(842,5,40,20);
-
-        l.setBorder(blackline);
-        l.setHorizontalAlignment(JLabel.CENTER);
-        l.setVerticalAlignment(JLabel.CENTER);
-        l.setFont(font);
 
 
-        f.add(l);
-        f.add(b);//adding button in JFrame
+
+        printMidLine();
+        printMainGridTopLabels();
+        printMainGridButtons();
+
 
     }
 
+    static void printMidLine() {
+        JLabel midLine = new JLabel();
+        midLine.setBounds(frameWidth/2, 0, 1, frameHeight);
+        midLine.setBorder(blackline);
+        GameBoard.f.add(midLine);
+    }
+
+    static void printMainGridTopLabels() {
+        int width = (frameWidth/2) + 35;
+        JLabel[] l = new JLabel[10];
+
+        for (int i = 0; i < 10; i++) {
+            l[i] = new JLabel(Character.toString((char) 65 + i));
+            l[i].setBounds(width, 35, 30, 20);
+            l[i].setBorder(blackline);
+            l[i].setHorizontalAlignment(JLabel.CENTER);
+            l[i].setVerticalAlignment(JLabel.CENTER);
+            l[i].setFont(font);
+            GameBoard.f.add(l[i]);
+            width = width + 30;
+        }
+    }
+
+   static void printMainGridButtons() {
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+
+                JButton b = new JButton("");
+                b.setBounds((frameWidth-58),30,25, 25);
+                b.setEnabled(false);
+                GameBoard.f.add(b);
+
+            }
+        }
+
+
+
+
+
+    }
 }
 
 
