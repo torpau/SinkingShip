5ba2d695b2e7e7be40eb24cf8f6af288e4cfc5c1
Torsten Pause <torsten.pause@student.kyh.se>
2020-10-14 12:41:18 +0200

    added left and right buttons

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -6,8 +6,10 @@ import java.awt.*;
 
 public class GameBoard {
     public static JFrame f = new JFrame();
-    private static final int frameWidth = 720;
-    private static final int frameHeight = 400;
+    private static final int frameWidth = 1110;
+    private static final int frameHeight = 560;
+    public static JButton[] leftB = new JButton[101];
+    public static JButton[] rightB = new JButton[101];
     private static final Font font = new Font("Verdana", Font.PLAIN, 12);
     private static final Border blackline = BorderFactory.createLineBorder(Color.black);
 
@@ -16,78 +18,130 @@ public class GameBoard {
     }
 
     static void initGameBoard() {
-        GameBoard.f.setBounds(200, 150, frameWidth, frameHeight);
-        GameBoard.f.setLayout(null);//using no layout managers
-        GameBoard.f.setVisible(true);//making the frame visible
-        //f.getJMenuBar();
+        //GameBoard.f.setBounds(0, 0, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+        f.setBounds(200, 150, frameWidth, frameHeight);
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);
 
-        JButton b = new JButton("");//creating instance of JButton
-        b.setBounds((frameWidth-58),30,25, 25);//x axis, y axis, width, height
-        b.setEnabled(false);
-
-
-
-
-        printMidLine();
         printLeftGridTopLabels();
+        printLeftGridSideLabels();
+        printLeftGridButtons();
+        printMidLine();
         printRightGridTopLabels();
-        printMainGridButtons();
+        printRightGridSideLabels();
+        printRightGridButtons();
 
+    }
 
+    static void printLeftGridTopLabels() {
+        int width = 79;
+        JLabel[] leftTL = new JLabel[10];
+
+        for (int i = 0; i < 10; i++) {
+            leftTL[i] = new JLabel(Character.toString((char) 65 + i));
+            leftTL[i].setBounds(width, 35, 44, 44);
+            leftTL[i].setBorder(blackline);
+            leftTL[i].setHorizontalAlignment(JLabel.CENTER);
+            leftTL[i].setVerticalAlignment(JLabel.CENTER);
+            leftTL[i].setFont(font);
+            f.add(leftTL[i]);
+            width = width + 44;
+        }
+    }
+
+    static void printLeftGridSideLabels() {
+        int width = 35;
+        int height = 79;
+        JLabel[] leftSL = new JLabel[10];
+
+        for (int i = 0; i < 10; i++) {
+            leftSL[i] = new JLabel("" + (i+1));
+            leftSL[i].setBounds(width, height, 44, 44);
+            leftSL[i].setBorder(blackline);
+            leftSL[i].setHorizontalAlignment(JLabel.CENTER);
+            leftSL[i].setVerticalAlignment(JLabel.CENTER);
+            leftSL[i].setFont(font);
+            f.add(leftSL[i]);
+            height = height + 44;
+        }
+    }
+
+    static void printLeftGridButtons() {
+        int width = 79;
+        int height = 79;
+
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                leftB[i*j] = new JButton();
+                leftB[i*j].setBounds(width,height,44, 44);
+                leftB[i*j].setEnabled(true);
+                leftB[i*j].setText("X");
+                f.add(leftB[i*j]);
+                width = width + 44;
+            }
+            width = 79;
+            height = height + 44;
+        }
     }
 
     static void printMidLine() {
         JLabel midLine = new JLabel();
         midLine.setBounds(frameWidth/2, 0, 1, frameHeight);
         midLine.setBorder(blackline);
-        GameBoard.f.add(midLine);
+        f.add(midLine);
     }
 
     static void printRightGridTopLabels() {
+        int width = (frameWidth/2) + 79;
+        JLabel[] rightTL = new JLabel[10];
+
+        for (int i = 0; i < 10; i++) {
+            rightTL[i] = new JLabel(Character.toString((char) 65 + i));
+            rightTL[i].setBounds(width, 35, 44, 44);
+            rightTL[i].setBorder(blackline);
+            rightTL[i].setHorizontalAlignment(JLabel.CENTER);
+            rightTL[i].setVerticalAlignment(JLabel.CENTER);
+            rightTL[i].setFont(font);
+            f.add(rightTL[i]);
+            width = width + 44;
+        }
+    }
+
+    static void printRightGridSideLabels() {
         int width = (frameWidth/2) + 35;
-        JLabel[] right = new JLabel[10];
+        int height = 79;
+        JLabel[] rightSL = new JLabel[10];
 
         for (int i = 0; i < 10; i++) {
-            right[i] = new JLabel(Character.toString((char) 65 + i));
-            right[i].setBounds(width, 35, 30, 20);
-            right[i].setBorder(blackline);
-            right[i].setHorizontalAlignment(JLabel.CENTER);
-            right[i].setVerticalAlignment(JLabel.CENTER);
-            right[i].setFont(font);
-            GameBoard.f.add(right[i]);
-            width = width + 30;
+            rightSL[i] = new JLabel("" + (i+1));
+            rightSL[i].setBounds(width, height, 44, 44);
+            rightSL[i].setBorder(blackline);
+            rightSL[i].setHorizontalAlignment(JLabel.CENTER);
+            rightSL[i].setVerticalAlignment(JLabel.CENTER);
+            rightSL[i].setFont(font);
+            f.add(rightSL[i]);
+            height = height + 44;
         }
     }
 
-    static void printLeftGridTopLabels() {
-        int width = 35;
-        JLabel[] left = new JLabel[10];
+    static void printRightGridButtons() {
+        int width = (frameWidth/2) + 79;
+        int height = 79;
 
-        for (int i = 0; i < 10; i++) {
-            left[i] = new JLabel(Character.toString((char) 65 + i));
-            left[i].setBounds(width, 35, 30, 20);
-            left[i].setBorder(blackline);
-            left[i].setHorizontalAlignment(JLabel.CENTER);
-            left[i].setVerticalAlignment(JLabel.CENTER);
-            left[i].setFont(font);
-            GameBoard.f.add(left[i]);
-            width = width + 30;
-        }
-    }
-
-   static void printMainGridButtons() {
-
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-
-                JButton b = new JButton("");
-                b.setBounds((frameWidth-58),30,25, 25);
-                b.setEnabled(false);
-                GameBoard.f.add(b);
-
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                rightB[i*j] = new JButton();
+                rightB[i*j].setBounds(width,height,44, 44);
+                rightB[i*j].setEnabled(true);
+                rightB[i*j].setText("X");
+                f.add(rightB[i*j]);
+                width = width + 44;
             }
+            width = (frameWidth/2) + 79;
+            height = height + 44;
         }
     }
+
 }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,10 @@ public class Main {
     public static void main(String[] args) {
         if(GameProgress.ifGameIsSaved()) { fetchSavedGameData(); }
         GameBoard.initGameBoard();
+        
+
+
+
     }
 
 
